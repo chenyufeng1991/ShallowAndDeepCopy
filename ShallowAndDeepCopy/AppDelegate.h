c7a39e8e8311decf2215ae19280562966d8a361d
陈宇峰 <yufengcode@gmail.com>
2016-06-22 19:27:54 +0800

    Initial Commit

---
+++ ShallowAndDeepCopy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShallowAndDeepCopy
+//
+//  Created by chenyufeng on 6/22/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
